4ab5ac9cd1c38f5191ababc0d55127b1f3d5365d
Bienn Viquiera <biennviquiera@gmail.com>
2023-06-24 10:02:57 -0700

    Used eslint on app.js and followed styling conventions

--- app/app.js
+++ app/app.js
@@ -1,32 +1,31 @@
-const express = require('express');
-const multer  = require('multer');
-const cors = require('cors');
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
-const ffmpeg = require('fluent-ffmpeg');
-ffmpeg.setFfmpegPath(ffmpegPath);
+import express from 'express'
+import multer from 'multer'
+import cors from 'cors'
+import ffmpegInstaller from '@ffmpeg-installer/ffmpeg'
+import ffmpeg from 'fluent-ffmpeg'
+ffmpeg.setFfmpegPath(ffmpegInstaller.path)
 
+const app = express()
+app.use(cors())
 
-const app = express();
-app.use(cors());
-
-var storage = multer.diskStorage({
+const storage = multer.diskStorage({
   destination: function (request, file, callback) {
-      callback(null, './uploads/');
+    callback(null, './uploads/')
   },
   filename: function (request, file, callback) {
-      console.log(file);
-      callback(null, file.originalname)
+    console.log(file)
+    callback(null, file.originalname)
   }
-});
+})
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage })
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on port ${port}`)
+})
 
-const processFile = (currentFile, outputPath, startTime, endTime, loopedPath) => {
+const processFile = (currentFile, outputPath, startTime, endTime) => {
   return new Promise((resolve, reject) => {
     ffmpeg()
       .input(currentFile.path)
@@ -34,9 +33,9 @@ const processFile = (currentFile, outputPath, startTime, endTime, loopedPath) =>
       .output(outputPath)
       .on('end', resolve)
       .on('error', reject)
-      .run();
-  });
-};
+      .run()
+  })
+}
 
 const loopFile = (outputPath, loopedPath) => {
   return new Promise((resolve, reject) => {
@@ -47,41 +46,41 @@ const loopFile = (outputPath, loopedPath) => {
       .output(loopedPath)
       .on('end', resolve)
       .on('error', reject)
-      .run();
-  });
-};
+      .run()
+  })
+}
 
 app.post('/api/loop', upload.single('myFile'), (req, res) => {
   console.log(req.body) // form fields
   console.log(req.file) // form files
-  const startTime = '00:24.500';
-  const endTime = '00:30.000';
-  const currentFile = req.file;
-  const outputPath = 'uploads/trimmed_' + currentFile.filename;
-  const loopedPath = 'uploads/looped_' + currentFile.filename;
+  const startTime = '00:24.500'
+  const endTime = '00:30.000'
+  const currentFile = req.file
+  const outputPath = 'uploads/trimmed_' + currentFile.filename
+  const loopedPath = 'uploads/looped_' + currentFile.filename
 
   processFile(currentFile, outputPath, startTime, endTime)
     .then(() => {
-      console.log('File trimming finished');
-      return loopFile(outputPath, loopedPath);
+      console.log('File trimming finished')
+      return loopFile(outputPath, loopedPath)
     })
     .then(() => {
-      console.log('File looping finished');
-      res.status(200).send('File successfully looped!');
+      console.log('File looping finished')
+      res.status(200).send('File successfully looped!')
     })
     .catch((err) => {
-      console.error(err);
+      console.error(err)
       if (!res.headersSent) {
-        res.status(500).send('Error during processing');
+        res.status(500).send('Error during processing')
       }
-    });
-});
+    })
+})
 
-app.post('/login', function(req, res){
-  console.log('test');
+app.post('/login', function (req, res) {
+  console.log('test')
   res.status(200).json({
-    status: "success",
-    timestamp: Date.now(),
-});
-  res.end(); // end the response
-});
+    status: 'success',
+    timestamp: Date.now()
+  })
+  res.end() // end the response
+})
