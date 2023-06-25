25f8c5b282c0bdbe57973787ef253526f80a201c
Bienn Viquiera <biennviquiera@gmail.com>
2023-06-25 14:00:03 -0700

    Implemented audio snippet preview and added slider to control selection of audio file to loop using rc-slider package. On change, the audio file previews where the snippet will start. TODO: use the right handle of slider to limit and initiate the loop

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,32 @@
 'use client'
 
-import Image from 'next/image';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+// import 'rc'
 import Layout from './layout';
-import { useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './globals.css';
-import { NextPageContext } from 'next';
 import { useRouter } from 'next/navigation';
 
-Home.getInitialProps = async (ctx: NextPageContext) => {
-  console.log("bruhmans");
-  const res = await fetch('https://api.github.com/repos/vercel/next.js')
-  const json = await res.json()
-  return { stars: json.stargazers_count }
-}
-
-export default function Home({ stars }: { stars: number }) {
+export default function Home() {
   const router = useRouter();
+  const [audioFile, setAudioFile] = useState<string | undefined>(undefined);
+  const audioElement = useRef<HTMLAudioElement | null>(null);
+
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
 
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [fileName, setFileName] = useState<String>("None");
 
-  const fileSelectedHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log('bruh');
+  const [currentTime, setCurrentTime] = useState<number>(0);
+  const [duration, setDuration] = useState<number>(0);
+  const [selectedRange, setSelectedRange] = useState<number[]>([0,0]);
 
+  const fileSelectedHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files[0]) {
       setSelectedFile(event.target.files[0]);
       setFileName(event.target.files[0].name);
+      setAudioFile(URL.createObjectURL(event.target.files[0]));
     }
   }
   
@@ -36,14 +37,69 @@ export default function Home({ stars }: { stars: number }) {
     if (selectedFile) {
       formData.append('myFile', selectedFile, selectedFile.name);
       console.log(...formData);
-
-      const res = fetch('http://localhost:3001/api/loop', {
-        method: 'POST',
-        body: formData,
-      });
+    //  // Used for sending to loop
+    //   const res = fetch('http://localhost:3001/api/loop', {
+    //     method: 'POST',
+    //     body: formData,
+    //   });
     }
   }
+
+  useEffect(() => {
+    if (audioElement.current) {
+      audioElement.current.ontimeupdate = () => {
+        if (audioElement.current) {
+          setCurrentTime(audioElement.current.currentTime);
+        }
+      };
+      audioElement.current.onloadedmetadata = () => {
+        if (audioElement.current) {
+          setDuration(audioElement.current.duration);
+          setSelectedRange([selectedRange[0], audioElement.current.duration])
+        }
+      };
+    }
+  }, [audioFile]);
   
+  const onProgressChange = (value: number | number[]) => {
+    console.log(value);
+    if (audioElement.current) {
+      if (typeof value === 'number') {
+        audioElement.current.currentTime = value;
+      } else if (value.length > 0) {
+        audioElement.current.currentTime = value[0];
+        setSelectedRange(value);
+      }
+    }
+  };
+
+  const togglePlayPause = () => {
+    if (audioElement.current) {
+      if (isPlaying) {
+        audioElement.current.pause();
+      } else {
+        audioElement.current.play();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
+  useEffect(() => {
+    const handleSpacebar = (event: KeyboardEvent) => {
+      if (audioElement.current) {
+        audioElement.current.currentTime = selectedRange[0];
+      } else {
+        console.error("audioElement.current is null");
+      } 
+      if (event.code === 'Space') {;
+        togglePlayPause();
+      }
+    }
+    window.addEventListener('keydown', handleSpacebar);
+    return () => {
+      window.removeEventListener('keydown', handleSpacebar);
+    };
+  }, [isPlaying]);
 
   return (
     <Layout>
@@ -51,9 +107,23 @@ export default function Home({ stars }: { stars: number }) {
         <h1 className="bg-blue-200 m-8 font-bold py-10 text-4xl text-center">MP3 Looper</h1>
         <div className="flex flex-col items-center justify-center">
           <input type="file" accept=".mp3" onChange={fileSelectedHandler} />
+          <audio ref={audioElement} src={audioFile} controls />
+
+          {audioElement && 
+          <Slider 
+            className="m-8"
+            range
+            min={0}
+            max={audioElement.current?.duration}
+            step={0.01}
+            onChange={onProgressChange}
+            draggableTrack={true}
+          />}
           <button className="bg-orange-200 rounded-md px-4 py-2 mt-4" onClick={fileUploadHandler}>Upload</button>
         </div>
       </div>
+
+
     </Layout>
   )
 }
